f444190e2067715f2f0d066f7481f500de9484c9
Björn Hansson <bjorn@bjornhansson.se>
2017-06-09 13:06:20 +0200

    Start with the backend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        function handleClick(e) {
+            e.preventDefault();
+            console.log('The link was clicked.');
+            fetch('https://api.github.com/users/github')
+            .then(res => res.json())
+            .then(json => console.log(json));
+        }
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <button className="App-check-backend" onClick={handleClick}>Check backend</button>
+            </div>
+        );
+    }
 }
 
 export default App;
