46c98bb4f3f42ffcfe2e0aeac6852a541aaedb1b
Björn Hansson <bjorn@bjornhansson.se>
2017-07-06 13:15:59 +0200

    Use the class concept

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
-function config() {
-  this.serverPort = 3001
-  this.clientPort = 3000
-  this.host = 'http://localhost:'
-  this.itemsEndpoint = '/items'
-  this.serverEndpoint = this.host + this.serverPort + this.itemsEndpoint
+class Config {
+    constructor() {
+        this.serverPort = 3001;
+        this.clientPort = 3000;
+        this.host = 'http://localhost:';
+        this.itemsEndpoint = '/items';
+        this.serverEndpoint = this.host + this.serverPort + this.itemsEndpoint;
+    }
 }
 
-module.exports = config;
+module.exports = Config;
